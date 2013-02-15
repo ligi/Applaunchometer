a73bd78bb36b2e4b4585848c4cbb9d926c0ec490
ligi <ligi@ligi.de>
2013-02-15 23:08:46 +0100

    prevent running app from being chosen

--- src/org/ligi/launchometer/Launchometer.java
+++ src/org/ligi/launchometer/Launchometer.java
@@ -1,12 +1,9 @@
 package org.ligi.launchometer;
 
 import android.app.Activity;
-import android.app.ActivityManager;
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -28,7 +25,12 @@ public class Launchometer extends Activity {
 
         List<ResolveInfo> allresolveInfoList = getPackageManager().queryIntentActivities(mainIntent, 0);
 
-        ResolveInfo activity2start_ri = allresolveInfoList.get((int) (Math.random() * allresolveInfoList.size()));
+        ResolveInfo activity2start_ri=null;
+
+        while(activity2start_ri==null || CheckService.isPkgRunning(this,activity2start_ri.activityInfo.packageName))
+            activity2start_ri = allresolveInfoList.get((int) (Math.random() * allresolveInfoList.size()));
+
+
         String label = activity2start_ri.loadLabel(getPackageManager()).toString() + " ";
         package2start = activity2start_ri.activityInfo.packageName;
         iv.setImageDrawable(activity2start_ri.loadIcon(getPackageManager()));
